982457bacab6cf7fde6b8ac857b4d824a19632e5
wanjala stephen <stephenwanjala145@gmail.com>
2022-10-28 13:08:18 +0300

    add tab layout

--- app/src/main/java/com/wantech/facebookcompose/featureHome/presentation/components/HomeScreen.kt
+++ app/src/main/java/com/wantech/facebookcompose/featureHome/presentation/components/HomeScreen.kt
@@ -1,18 +1,69 @@
 package com.wantech.facebookcompose.featureHome.presentation.components
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CardGiftcard
-import androidx.compose.material.icons.rounded.*
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material.icons.rounded.People
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.rememberPagerState
 import com.wantech.facebookcompose.R
+
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun HomeScreen(navHostController: NavHostController) {
+    val coroutineScope = rememberCoroutineScope()
+    val tabItems = listOf(
+        TabbedTabItem(
+            icon = Icons.Rounded.Home,
+            name = "Home",
+            route = "home",
+
+            ),
+        TabbedTabItem(
+            icon = Icons.Rounded.People,
+            name = "friends",
+            route = "find friend",
+
+            ),
+        TabbedTabItem(
+            icon = Icons.Rounded.Home,
+            name = "Feed",
+            route = "feed",
+            painterDrawable = R.drawable.fb_stores
+
+        ),
+        TabbedTabItem(
+            icon = Icons.Default.CardGiftcard,
+            name = "MarketPlace",
+            route = "marketplace",
+
+            ),
+        TabbedTabItem(
+            icon = Icons.Rounded.Notifications,
+            name = "Notifications",
+            route = "notifications",
+
+            ),
+        TabbedTabItem(
+            icon = Icons.Rounded.Menu,
+            name = "Notifications",
+            route = "notifications",
+
+            ),
+    )
     Surface(
         modifier = Modifier.fillMaxWidth(),
         color = MaterialTheme.colors.background
@@ -24,53 +75,23 @@ fun HomeScreen(navHostController: NavHostController) {
                 onClickSearch = { /*TODO*/ }) {
 
             }
+
+            val pagerState = rememberPagerState()
             Spacer(modifier = Modifier.height(8.dp))
-            TabbedArea(modifier = Modifier,
-                tabItems = listOf(
-                    TabbedTabItem(
-                        icon = Icons.Rounded.Home,
-                        name = "Home",
-                        route = "home",
+            TabbedArea(
+                modifier = Modifier,
+                tabItems = tabItems,
+                onClickTab = {
 
-                    ),
-                    TabbedTabItem(
-                        icon = Icons.Rounded.People,
-                        name = "friends",
-                        route = "find friend",
-
-                    ),
-                    TabbedTabItem(
-                        icon = Icons.Rounded.Home,
-                        name = "Home",
-                        route = "home",
-                        painterDrawable = R.drawable.fb_stores
-
-                    ),
-                    TabbedTabItem(
-                        icon = Icons.Default.CardGiftcard,
-                        name = "Home",
-                        route = "home",
-
-                    ),
-                    TabbedTabItem(
-                        icon = Icons.Rounded.Notifications,
-                        name = "Notifications",
-                        route = "notifications",
-
-                    ),
-                    TabbedTabItem(
-                        icon = Icons.Rounded.Menu,
-                        name = "Notifications",
-                        route = "notifications",
-
-                    ),
-                ),
-                onClickTab = {})
+                },
+                pagerState = pagerState
+            )
             Spacer(modifier = Modifier.height(16.dp))
-            StartArea(modifier = Modifier,
-                postImage = { /*TODO*/ },
-                viewProfile = { /*TODO*/ },
-                postSomeText = {/*TODO*/ })
+
+//            StartArea(modifier = Modifier,
+//                postImage = { /*TODO*/ },
+//                viewProfile = { /*TODO*/ },
+//                postSomeText = {/*TODO*/ })
         }
 
 
--- app/src/main/java/com/wantech/facebookcompose/featureHome/presentation/components/TabbedArea.kt
+++ app/src/main/java/com/wantech/facebookcompose/featureHome/presentation/components/TabbedArea.kt
@@ -1,7 +1,7 @@
 package com.wantech.facebookcompose.featureHome.presentation.components
 
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.interaction.collectIsDraggedAsState
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.runtime.*
@@ -9,60 +9,94 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.pager.*
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun TabbedArea(
     modifier: Modifier,
     tabItems: List<TabbedTabItem>,
-    onClickTab: (TabbedTabItem) -> Unit
+    onClickTab: (TabbedTabItem) -> Unit,
+    pagerState: PagerState
 ) {
     var selectIndex by remember {
         mutableStateOf(0)
     }
-    TabRow(
-        selectedTabIndex = selectIndex,
-        backgroundColor = MaterialTheme.colors.background,
-        contentColor = MaterialTheme.colors.onBackground,
-        divider = {
-            TabRowDefaults.Divider(
-                Modifier.wrapContentSize(Alignment.BottomStart),
-            )
-        },
-        indicator = {
-            TabRowDefaults.Indicator(
-                modifier = Modifier
-                    .tabIndicatorOffset(it[selectIndex]),
-                color = MaterialTheme.colors.primary,
-                height = TabRowDefaults.IndicatorHeight * 1.5F
-            )
+    Column() {
+
+
+
+        val tabIndex = pagerState.currentPage
+        val coroutineScope = rememberCoroutineScope()
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            backgroundColor = MaterialTheme.colors.background,
+            contentColor = MaterialTheme.colors.onBackground,
+            divider = {
+                TabRowDefaults.Divider(
+                    Modifier.wrapContentSize(Alignment.BottomStart),
+                )
+            },
+            indicator = {
+                TabRowDefaults.Indicator(
+                    color = MaterialTheme.colors.primary,
+                    height = TabRowDefaults.IndicatorHeight * 1.5F,
+                    modifier = Modifier
+//                        .tabIndicatorOffset(it[selectIndex])
+                        .pagerTabIndicatorOffset(pagerState, it)
+                )
+            }
+
+        ) {
+            tabItems.forEachIndexed { index, tabItem ->
+                Tab(
+                    selected = selectIndex == index,
+                    onClick = {
+                        selectIndex = index
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(selectIndex)
+
+                        }
+                    },
+                    unselectedContentColor = MaterialTheme.colors.onBackground,
+                    selectedContentColor = MaterialTheme.colors.primary,
+//                  interactionSource =
+
+
+                    ) {
+                    if (tabItem.painterDrawable != null) {
+                        Icon(
+                            painter = painterResource(id = tabItem.painterDrawable),
+                            contentDescription = tabItem.name,
+                            modifier = Modifier.size(40.dp)
+                        )
+                    } else {
+                        Icon(
+                            imageVector = tabItem.icon, contentDescription = tabItem.name,
+                            modifier = Modifier.size(40.dp)
+                        )
+                    }
+
+                }
+            }
+
         }
 
-    ) {
-        tabItems.forEachIndexed { index, tabItem ->
-            Tab(
-                selected = selectIndex == index,
-                onClick = {
-                    selectIndex = index
-                },
-                unselectedContentColor = MaterialTheme.colors.onBackground,
-                selectedContentColor = MaterialTheme.colors.primary,
-
-                ) {
-                if (tabItem.painterDrawable != null) {
-                    Icon(
-                        painter = painterResource(id = tabItem.painterDrawable),
-                        contentDescription = tabItem.name,
-                        modifier = Modifier.size(40.dp)
-                    )
-                } else {
-                    Icon(
-                        imageVector = tabItem.icon, contentDescription = tabItem.name,
-                        modifier = Modifier.size(40.dp)
-                    )
-                }
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.weight(1f),
+            count = tabItems.size
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
 
             }
-        }
 
+        }
     }
 }
